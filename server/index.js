00c48b88b2054af251c33ec2116d76d3142a62b2
Dylan Schwindt <dfieldmark69@gmail.com>
2021-08-27 09:11:33 -0700

    setting up server for heroku

--- server/index.js
+++ server/index.js
@@ -3,12 +3,16 @@ const path = require('path')
 
 const app = express()
 
-app.get('/',function(req,res) {
-  res.sendFile(path.join(__dirname, '../tictacjs.html'));
-});
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname, '../index.html'))
+})
+
+app.get('/css', function (req, res) {
+	res.sendFile(path.join(__dirname, '../styles.css'))
+})
 
 const port = process.env.PORT || 4000
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
+	console.log(`Listening on port ${port}`)
 })
